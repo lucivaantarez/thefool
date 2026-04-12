a4246cac01746ccdf7c7e1a7fad2026b57f6a98f
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-12 11:55:04 +0700

    Update homebase.lua

--- homebase.lua
+++ homebase.lua
@@ -12,8 +12,20 @@ local http_request = syn and syn.request or http and http.request or http_reques
 
 task.spawn(function()
     while true do
-        local payload = {userid = tostring(Players.LocalPlayer.UserId), jobid = tostring(game.JobId), role = MY_ROLE, players = #Players:GetPlayers()}
-        pcall(function() http_request({Url = HUB_URL, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = HttpService:JSONEncode(payload)}) end)
+        local payload = {
+            userid = tostring(Players.LocalPlayer.UserId), 
+            jobid = tostring(game.JobId), 
+            role = MY_ROLE, 
+            players = #Players:GetPlayers()
+        }
+        pcall(function() 
+            http_request({
+                Url = HUB_URL, 
+                Method = "POST", 
+                Headers = {["Content-Type"] = "application/json"}, 
+                Body = HttpService:JSONEncode(payload)
+            }) 
+        end)
         task.wait(60)
     end
 end)
