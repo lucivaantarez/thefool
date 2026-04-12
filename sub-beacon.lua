65d70d59bf9b3699948983e67381145ac0eeb63a
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-12 11:45:14 +0700

    Create sub-beacon.lua

---
+++ sub-beacon.lua
@@ -0,0 +1,16 @@
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+
+local NGROK_URL = "https://YOUR-URL.trycloudflare.com/api/ping"
+
+if not game:IsLoaded() then game.Loaded:Wait() end
+task.wait(5)
+local http_request = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or request
+
+task.spawn(function()
+    while true do
+        local payload = {userid = tostring(Players.LocalPlayer.UserId), jobid = tostring(game.JobId), role = "TARGET", players = #Players:GetPlayers()}
+        pcall(function() http_request({Url = NGROK_URL, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = HttpService:JSONEncode(payload)}) end)
+        task.wait(60) 
+    end
+end)
