e15659186f6eb922d96eea721201896a9786ab89
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-12 14:14:35 +0700

    Create trade.lua

---
+++ trade.lua
@@ -0,0 +1,19 @@
+script_key="YrMGlHVZJYWpvtBaeKtZJOAtEBmcQbkQ";
+setfpscap(10)
+
+getgenv().sailorPieceConfig = {  
+    OPTIMIZATION = false, 
+    AUTO_KICK = faalse,
+    TRADE_USERNAME = {},  -- "aduhhhbrisik"
+    TRADE_ITEM = {},
+
+    WEBHOOK_URL = "https://discord.com/api/webhooks/1481617340247576729/oNNhsQuK_3MoXArfADV6fv5_xk4nIuxMWPCarj5c-_fw6nav2BtOM60xh5232tegePUm",
+    DISCORD_ID = "285071154300059648",
+    WEBHOOK_NOTE = "",
+    SHOW_WEBHOOK_USERNAME = true,
+    SHOW_WEBHOOK_JOBID = true,
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/eb9a467b35fe098d20677eb16ec559a4.lua"))()
+```
+⚠️ Do not include **Malevolent key, Limitless key, Cursed Talisman, Slime Key** in `TRADE_ITEMS` to prevent unlimited trading, each account can only hold 500 of it.
