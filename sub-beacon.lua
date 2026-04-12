8e89d22aa230038b1056c2190fc84b445a877da0
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-12 11:55:42 +0700

    Update sub-beacon.lua

--- sub-beacon.lua
+++ sub-beacon.lua
@@ -11,8 +11,20 @@ local http_request = syn and syn.request or http and http.request or http_reques
 
 task.spawn(function()
     while true do
-        local payload = {userid = tostring(Players.LocalPlayer.UserId), jobid = tostring(game.JobId), role = "TARGET", players = #Players:GetPlayers()}
-        pcall(function() http_request({Url = HUB_URL, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = HttpService:JSONEncode(payload)}) end)
+        local payload = {
+            userid = tostring(Players.LocalPlayer.UserId), 
+            jobid = tostring(game.JobId), 
+            role = "TARGET", 
+            players = #Players:GetPlayers()
+        }
+        pcall(function() 
+            http_request({
+                Url = HUB_URL, 
+                Method = "POST", 
+                Headers = {["Content-Type"] = "application/json"}, 
+                Body = HttpService:JSONEncode(payload)
+            }) 
+        end)
         task.wait(60) 
     end
 end)
