9316caa255a942933b58214d0af7944c81c18bf6
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-12 11:54:03 +0700

    Update sub-beacon.lua

--- sub-beacon.lua
+++ sub-beacon.lua
@@ -1,9 +1,9 @@
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 
--- *** UPDATE THIS LINK ***
-local NGROK_URL = "https://YOUR-URL.trycloudflare.com/api/ping"
--- ************************
+-- *** UPDATE THIS LINK FROM CLOUDFLARE ***
+local HUB_URL = "https://YOUR-URL.trycloudflare.com/api/ping"
+-- ***************************************
 
 if not game:IsLoaded() then game.Loaded:Wait() end
 task.wait(5)
@@ -12,7 +12,7 @@ local http_request = syn and syn.request or http and http.request or http_reques
 task.spawn(function()
     while true do
         local payload = {userid = tostring(Players.LocalPlayer.UserId), jobid = tostring(game.JobId), role = "TARGET", players = #Players:GetPlayers()}
-        pcall(function() http_request({Url = NGROK_URL, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = HttpService:JSONEncode(payload)}) end)
+        pcall(function() http_request({Url = HUB_URL, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = HttpService:JSONEncode(payload)}) end)
         task.wait(60) 
     end
 end)
