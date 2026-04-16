24329c66d2beee7b913e3417349eeee7d578e224
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-16 16:47:51 +0700

    Increase item limits in sailorPieceConfig

--- receiver.lua
+++ receiver.lua
@@ -2,7 +2,7 @@ script_key="YrMGlHVZJYWpvtBaeKtZJOAtEBmcQbkQ";
 setfpscap(10)
 
 getgenv().sailorPieceConfig = {  
-    KICK_ITEM_LIMIT = { ["Secret Chest"] = 99999999, ["Cosmetic Chest"] = 99999999, ["Aura Chest"] = 99999999 }
+    KICK_ITEM_LIMIT = { ["Secret Chest"] = 99999999999999, ["Cosmetic Chest"] = 999999999999999, ["Aura Chest"] = 9999999999999999 }
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d63b8de750f34c5a2bc6920f3849c318.lua"))()
