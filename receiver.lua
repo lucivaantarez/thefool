257249ae8b080367b8d0eb29183886879c000c1d
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-16 08:18:59 +0700

    Delete receiver.lua

--- receiver.lua
+++
@@ -1,8 +0,0 @@
-script_key="YrMGlHVZJYWpvtBaeKtZJOAtEBmcQbkQ";
-setfpscap(10)
-
-getgenv().sailorPieceConfig = {  
-    KICK_ITEM_LIMIT = { ["Secret Chest"] = 99999999, ["Cosmetic Chest"] = 99999999, ["Aura Chest"] = 99999999 }
-}
-
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d63b8de750f34c5a2bc6920f3849c318.lua"))()
