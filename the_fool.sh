3d53a58ac43fa6e0c7e5dc26ee4ea97c8bb5afe5
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-16 08:40:22 +0700

    Update the_fool.sh

--- the_fool.sh
+++ the_fool.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 
 # ── CONSTANTS ────────────────────────────────────────────────────────
 readonly VERSION="1.0.0"
-readonly GITHUB_RAW_URL="https://raw.githubusercontent.com/lucivaantarez/the-fool/main/the_fool.sh"
+readonly GITHUB_RAW_URL="https://raw.githubusercontent.com/lucivaantarez/thefool/main/the_fool.sh"
 readonly API_ENDPOINT="https://universe-vault.vercel.app/api/links"
 readonly API_AUTH_TOKEN="fool-secret-token-2025"
 readonly SELF_PATH="$(realpath "$0")"
