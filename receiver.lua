e237f5456c42dd478cf4624f94241284ff77c21a
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-19 13:59:07 +0700

    Update receiver.lua

--- receiver.lua
+++ receiver.lua
@@ -2,7 +2,14 @@ script_key="YrMGlHVZJYWpvtBaeKtZJOAtEBmcQbkQ";
 setfpscap(10)
 
 getgenv().sailorPieceConfig = {  
-    KICK_ITEM_LIMIT = { ["Secret Chest"] = 9999, ["Cosmetic Chest"] = 9999, ["Aura Chest"] = 9999 }
+    KICK_ITEM_LIMIT = { 
+        ["Secret Chest"] = 9999, 
+        ["Cosmetic Chest"] = 9999, 
+        ["Aura Chest"] = 9999,
+        ["Clan Reroll"] = 1999999,
+        ["Trait Reroll"] = 4999999,
+        ["Race Reroll"] = 4999999,
+    },
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d63b8de750f34c5a2bc6920f3849c318.lua"))()
